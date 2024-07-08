89b0623471a25db73623d8bdc97f35b206de5efc
Patel Shreyash Ashokbhai <160259841+shreyashpatel5506@users.noreply.github.com>
2024-07-08 13:28:10 +0000

    Sync LeetCode: Runtime - 60 ms (6.52%), Memory - 48.9 MB (100.00%)

--- solutions/2732-counter/solution.js
+++ solutions/2732-counter/solution.js
@@ -3,17 +3,7 @@
  * @return {Function} counter
  */
 var createCounter = function(n) {
-    
     return function() {
-        
     return n++
-    
-    };
+    }
 };
-
-/** 
- * const counter = createCounter(10)
- * counter() // 10
- * counter() // 11
- * counter() // 12
- */

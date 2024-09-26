605fa36c844d0eee9cbe5c0cfff8734256896784
Patel Shreyash Ashokbhai <160259841+shreyashpatel5506@users.noreply.github.com>
2024-09-26 13:53:23 +0000

    Sync LeetCode: Runtime - 46 ms (33.43%), Memory - 49.1 MB (100.00%)

---
+++ solutions/0374-guess-number-higher-or-lower/solution.js
@@ -0,0 +1,28 @@
+/** 
+ * Forward declaration of guess API.
+ * @param {number} num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * var guess = function(num) {}
+ */
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var guessNumber = function(n) {
+    
+      let start = 1
+    let end = n
+    while(start <= end){
+        let mid = Math.floor((start + end)/2)
+        if(guess(mid) == 0){
+            return mid
+        }else if( guess(mid) === 1 ){
+            start = mid + 1
+        }else{
+            end = mid - 1
+        }
+    }
+};

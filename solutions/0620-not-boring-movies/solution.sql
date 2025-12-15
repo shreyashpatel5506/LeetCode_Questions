78116b3334bdb9265f9659b8ca0be848221b0ca2
Patel Shreyash Ashokbhai <160259841+shreyashpatel5506@users.noreply.github.com>
2025-12-15 18:25:51 +0000

    Sync LeetCode: Runtime - 229 ms (100.00%), Memory - 0.0B (100.00%)

--- solutions/0620-not-boring-movies/solution.sql
+++ solutions/0620-not-boring-movies/solution.sql
@@ -1,2 +1,3 @@
-
-select * from Cinema where id%2=1 and description != 'boring' order by rating desc;
+# Write your MySQL query statement below
+select id,movie,description,rating from Cinema where id % 2 != 0
+  AND description <> 'boring' ORDER BY rating DESC;

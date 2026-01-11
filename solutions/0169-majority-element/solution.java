b4055da7e68791d2bce0c6cb7823949d949ec8fe
Patel Shreyash Ashokbhai <160259841+shreyashpatel5506@users.noreply.github.com>
2026-01-11 07:50:37 +0000

    Sync LeetCode: Runtime - 19 ms (11.77%), Memory - 52.5 MB (85.78%)

--- solutions/0169-majority-element/solution.java
+++ solutions/0169-majority-element/solution.java
@@ -1,8 +1,16 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        int m = nums.length / 2;
-        return nums[m];
-    }
-}
+     HashMap<Integer , Integer> hash = new HashMap<>();
+     int res=0;
+     int majority=0;
 
+     for(int n : nums){
+        hash.put(n,1+hash.getOrDefault(n,0));
+        if(hash.get(n)>majority){
+            res=n;
+            majority=hash.get(n);
+        }
+     }
+        return res;
+     }
+}

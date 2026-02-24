0872be4c04dbe49363e7a3505cee6f0c29efd1a6
Patel Shreyash Ashokbhai <160259841+shreyashpatel5506@users.noreply.github.com>
2026-02-24 18:36:09 +0000

    Sync LeetCode: Runtime - 7 ms (13.39%), Memory - 46.4 MB (6.76%)

---
+++ solutions/0009-palindrome-number/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        String s = Integer.toString(x);
+        int len = s.length();
+        for (int i = 0; i < len / 2; i++) {
+            if (s.charAt(i) != s.charAt(len - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}

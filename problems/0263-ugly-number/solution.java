14e3438cf70448a6a10aba93b6e6d0ae4359ff5c
shreyashpatel <shreyashpatel5506@gmail.com>
2026-02-28 11:29:22 +0000

    feat(leetcode): add 0263 Ugly Number

---
+++ problems/0263-ugly-number/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isUgly(int n) {
+        if(n<=0) return false;
+
+       int[] factors = {2, 3, 5};
+        for (int factor : factors) {
+            while (n % factor == 0) {
+                n /= factor;
+            }
+        }
+        if(n==1){
+            return true;
+        }
+        return false;
+    }
+}

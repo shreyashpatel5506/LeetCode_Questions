60a373213d17ba5b2303cf9b28f17e0382d74462
Patel Shreyash Ashokbhai <160259841+shreyashpatel5506@users.noreply.github.com>
2026-04-04 14:09:03 +0000

    Sync LeetCode: Runtime - 769 ms (65.97%), Memory - 0.0B (100.00%)

--- solutions/1664-find-users-with-valid-e-mails/solution.sql
+++ solutions/1664-find-users-with-valid-e-mails/solution.sql
@@ -1,3 +1,9 @@
-SELECT *
-FROM Users
-WHERE mail REGEXP '^[A-Za-z][A-Za-z0-9_\.\-]*@leetcode(\\?com)?\\.com$';
+SELECT 
+    user_id, 
+    name, 
+    mail
+FROM 
+    users
+WHERE 
+    mail REGEXP '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode\\.com$' 
+    AND mail LIKE BINARY '%@leetcode.com';

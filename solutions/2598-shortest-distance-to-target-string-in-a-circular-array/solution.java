eeda24699f061fc5cf6efdc01002c95086e2a313
Patel Shreyash Ashokbhai <160259841+shreyashpatel5506@users.noreply.github.com>
2026-04-15 17:14:45 +0000

    Sync LeetCode: Runtime - 0 ms (100.00%), Memory - 46.7 MB (69.43%)

---
+++ solutions/2598-shortest-distance-to-target-string-in-a-circular-array/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int closestTarget(String[] words, String target, int startIndex) {
+        int n = words.length;
+        int minDistance = Integer.MAX_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            if (words[i].equals(target)) {
+          
+                int directDist = Math.abs(i - startIndex);
+        
+                int circularDist = n - directDist;
+                
+           
+                int currentMin = Math.min(directDist, circularDist);
+                
+          
+                minDistance = Math.min(minDistance, currentMin);
+            }
+        }
+
+        return (minDistance == Integer.MAX_VALUE) ? -1 : minDistance;
+    }
+}

57a3040354424cfc609fef183c349cf9cc3176ca
shreyashpatel <shreyashpatel5506@gmail.com>
2026-05-04 10:04:57 +0000

    feat(leetcode): add 0824 Goat Latin

---
+++ problems/0824-goat-latin/README.md
@@ -0,0 +1,16 @@
+# Goat Latin
+
+## Details
+- **Problem ID:** 0824
+- **Difficulty:** Easy
+- **Topics:** String
+- **Language:** java
+- **LeetCode Link:** [LeetCode - Goat Latin](https://leetcode.com/problems/goat-latin/)
+- **Last Updated:** 2026-05-04
+
+## Folder Contents
+- [Solution File](./solution.java)
+
+<!-- AI_EXPLANATION_START -->
+*No explanation added yet. AI explanations can be generated here in the future.*
+<!-- AI_EXPLANATION_END -->
---
+++ problems/0824-goat-latin/solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String toGoatLatin(String sentence) {
+        String[] words = sentence.split(" ");
+        StringBuilder result = new StringBuilder();
+        String vowels = "aeiouAEIOU";
+        
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            char firstChar = word.charAt(0);
+            if (vowels.indexOf(firstChar) != -1) {
+                result.append(word);
+            } else {
+                result.append(word.substring(1)).append(firstChar);
+            }
+            result.append("ma");
+            for (int j = 0; j <= i; j++) {
+                result.append("a");
+            }
+            if (i < words.length - 1) {
+                result.append(" ");
+            }
+        }
+        
+        return result.toString();
+    }
+}
